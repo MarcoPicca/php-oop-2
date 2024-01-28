54d0a5906df62f7ed6118ebf907465d8153cfc5e
MarcoPicca <il96marco@gmail.com>
2024-01-28 14:59:03 +0100

    style.css

--- index.php
+++ index.php
@@ -16,12 +16,17 @@ include_once 'Products/Product.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
     <!-- css  -->
     <link rel="stylesheet" href="style/style.css">
+    
     <!-- font  -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@700&display=swap" rel="stylesheet">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
 
@@ -32,13 +37,16 @@ include_once 'Products/Product.php';
             <div class="col-12 container d-flex justify-content-around ">
                     <img class="rounded-circle" src="https://img.freepik.com/premium-vector/colorful-bird-illustration-gradient-abstract_343694-1740.jpg" width="60" height="60" alt="">
                     <h1 class="text-white text-uppercase font-weight-bold">Pet Store</h1>
+                    <div class="burger-menu">
+                        <i class="fa-solid text-white fa-bars fa-xl"></i>
+                    </div>
             </div>
         </nav>
     </header>
 
 
     
-    <main class="d-flex bg-info flex-wrap pt-5 justify-content-center">
+    <main class="d-flex flex-wrap pt-5 justify-content-center">
         <?php foreach ($Products as $product) { ?>
             <div class="col-2 m-5 mb-2 d-flex flex-direction-column align-items-stretch">
                 <div class="card">
